7bda390cb29be7a93819056ed99bea45b9705d95
VincentDC123 <vincentdc5656@gmail.com>
2023-10-10 16:18:03 -0700

    Added Delete, Remove, and All functions. Added error checks.

--- infinityBot.py
+++ infinityBot.py
@@ -51,9 +51,10 @@ async def on_message(message):
         await message.channel.send('Happy Birthday!')
         await message.channel.send(message.content)
 
+    # Help
     if '!todo help' in message.content.lower():
         await message.channel.send(
-            'Available Commands: \n !todo create (name) \n\t-Creates a To Do List. \n !todo add (name) (item) \n\t-Adds item to a specific To Do List \n !todo check (name) \n\t-Checks a specific To Do List'
+            'Available Commands: \n !todo create (name) \n\t-Creates a To Do List. \n !todo add (name) (item) \n\t-Adds item to a specific To Do List \n !todo check (name) \n\t-Checks a specific To Do List \n!todo delete (name) (number) \n\t-Deletes a specific item in the To Do List \n!todo remove (name) \n\t-Removes a specific list \n!todo all \n\t-Checks all the Todo List'
                                    )
     # Creates a List
     elif '!todo create' in message.content.lower():
@@ -66,15 +67,42 @@ async def on_message(message):
         temp = ""
         for x in range(3, len(message_array)):
             temp += message_array[x] + " "
-        todoList[message_array[2]].append(temp)
-        await message.channel.send(todoList[message_array[2]])
+        if message_array[2] in todoList:
+            todoList[message_array[2]].append(temp)
+            await message.channel.send(todoList[message_array[2]])
+        else:
+            await message.channel.send('List does not exist.')
     # Checks a specific List
     elif '!todo check' in message.content.lower():
         message_array = message.content.lower().split(' ')
         index = 1
-        await message.channel.send(message_array[2] + " To-Do List:\n")
-        for x in range(len(todoList[message_array[2]])):
-            await message.channel.send(f'{index}) {todoList[message_array[2]][x]}')
-            index += 1
+        if message_array[2] in todoList:
+            await message.channel.send(message_array[2] + " To-Do List:\n")
+            for x in range(len(todoList[message_array[2]])):
+                await message.channel.send(f'{index}) {todoList[message_array[2]][x]}')
+                index += 1
+        else:
+            await message.channel.send('List does not exist.')
+    # Deletes a specific item in the specific List
+    elif '!todo delete' in message.content.lower():
+        message_array = message.content.lower().split(' ')
+        try:
+            temp = todoList[message_array[2]].pop(int(message_array[3])-1)
+            await message.channel.send('Deleting the item ' + temp)
+        except ValueError:
+            await message.channel.send('Item in the To Do List does not exist.')
+        except IndexError:
+            await message.channel.send('Item in the To Do List does not exist.')
+    # Removes the entire list in the To Do List
+    elif '!todo remove' in message.content.lower():
+        message_array = message.content.lower().split(' ')
+        try:
+            todoList.pop(message_array[2],None)
+            await message.channel.send('Deleting the To Do List ' + message_array[2] + '.')
+        except KeyError:
+            await message.channel.send('List does not exist.')
+    # Checks all To Do Lists
+    elif '!todo all' in message.content.lower():
+        await message.channel.send(todoList)
 
 client.run(TOKEN)
